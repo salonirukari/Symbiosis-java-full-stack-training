8fc18998fa6651c84a4629d227aa87409a24357c
salonirukari <rukarisaloni@gmail.com>
2024-08-28 13:20:41 +0530

    Add files via upload

---
+++ mathematical_operation.java
@@ -0,0 +1,29 @@
+package demo;
+import java.util.*;
+
+public class mathematical_operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int first, second, add, subtract, multiply;
+	    float devide;
+	    Scanner scanner = new Scanner(System.in);
+
+	    System.out.print("Enter Two Numbers : ");
+	    first = scanner.nextInt();
+	    second = scanner.nextInt();
+
+	    add = first + second;
+	    subtract = first - second;
+	    multiply = first * second;
+	    devide = (float) first / second;
+
+	    System.out.println("Sum = " + add);
+	    System.out.println("Difference = " + subtract);
+	    System.out.println("Multiplication = " + multiply);
+	    System.out.println("Division = " + devide);
+	  }
+
+	}
+
+
