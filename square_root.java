6d48a0eb46ca860f3d202a4483e04ce170e85020
salonirukari <rukarisaloni@gmail.com>
2024-08-31 12:49:56 +0530

    Add files via upload

---
+++ square_root.java
@@ -0,0 +1,19 @@
+package demo;
+import java.util.*;
+
+public class square_root {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter a number: ");  
+        int num = sc.nextInt();  
+        Double squareroot = Math.pow(num, 0.5);
+        System.out.println("The Square Root of the Given Number  " + num + "  =  " + squareroot);
+ 
+   }  
+  
+
+	}
+
+
