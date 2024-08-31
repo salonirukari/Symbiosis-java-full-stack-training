2bca6bac43a460d625dfb89a2bf76508e0673484
salonirukari <rukarisaloni@gmail.com>
2024-08-31 12:46:41 +0530

    Add files via upload

---
+++ C_S_INTEREST.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class C_S_INTEREST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Principal = 9000: ");
+	    double principal = 9000;
+
+	    System.out.print("rate of interest = 10 : ");
+	    double rate = 10;
+
+	    System.out.print("number of years = 5 : ");
+	    int years = 5;
+
+	    double simpleInterest = (principal * rate * years) / 100;
+	    System.out.println("Simple Interest: " + simpleInterest);
+	  
+	
+
+
+	}
+
+}
---
+++ ascii.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'a';
+        int ascii = ch;
+     
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    
+
+	}
+
+}
