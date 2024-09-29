cf44f3c486f695aaf2621ec0c886a887dd86db21
salonirukari <rukarisaloni@gmail.com>
2024-09-29 23:37:38 +0530

    Add files via upload

--- retirement.java
+++ retirement.java
@@ -1,55 +1,20 @@
-package web;
+package screen;
 
-import java.util.Hashtable;
-import java.util.Scanner;
-class Student{
+import java.time.LocalDate;
 
-public class retirement {
-	 private String name;
-	private String dateOfBirth;
-	private String course;
+public class retirement 
+{
+	  public static void main(String[] args) {
+	        // Input date of joining (yyyy-MM-dd)
+	        String joiningDate = "1950-01-01";
 
-	public void Student(String dateOfBirth, String name, String course) {
-	        this.dateOfBirth = dateOfBirth;
-	        this.name = name;
-	        this.course = course;
+	        // Convert joining date to LocalDate object
+	        LocalDate localJoiningDate = LocalDate.parse(joiningDate);
+
+	        // Calculate retirement date by adding 58 years
+	        LocalDate retirementDate = localJoiningDate.plusYears(58);
+
+	        // Print the calculated retirement date
+	        System.out.println("Retirement Date: " + retirementDate);
 	    }
-
-	    @Override
-	    public String toString() {
-	        return "Name: " + name + ", Date of Birth: " + dateOfBirth + ", Course: " + course;
-	    }
-	}
-	public class hashtable {
-	    public static void main(String[] args) {
-	        Hashtable<Integer, Student> students = new Hashtable<>();
-	        Scanner sc = new Scanner(System.in);
-	        System.out.print("Enter number of students: ");
-	        int numberOfStudents = sc.nextInt();
-	        sc.nextLine();
-
-	        for (int i = 0; i < numberOfStudents; i++) {
-	            System.out.print("Enter roll number: ");
-	            int rollNo = sc.nextInt();
-	            sc.nextLine(); 
-
-	            System.out.print("Enter date of birth (YYYY-MM-DD):");
-	            String dateOfBirth = sc.nextLine();
-
-	            System.out.print("Enter name: ");
-	            String name = sc.nextLine();
-
-	            System.out.print("Enter course enrolled: ");
-	            String course = sc.nextLine();
-
-	            students.put(rollNo, new Student());
-	        }
-	        System.out.println("\nStudent Data in Hashtable:");
-	        for (Integer rollNo : students.keySet()) {
-	            System.out.println("Roll No: " + rollNo + ", " + students.get(rollNo));
-	        }
-	           sc.close();
-	    }
-	}
-
 }
